aeff5db93b37b6fe826c13da5b54b60e989ca7b0
Nikhil Sangamkar <nsangamkar1222@gmail.com>
2026-01-13 17:39:42 -0500

    sound remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,13 @@ import ExperienceSection from "@/components/experience";
 import Projects from "@/components/projects";
 import ContactForm from "@/components/contact";
 import Footer from "@/components/footer";
-import Sound from "@/components/sound";
 import EducationSection from "@/components/education";
 import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main className="bg-cyan-950 text-white">
-      {/* <Sound /> */}
+      
       <Navbar />
       <Hero />
       <ExperienceSection />
